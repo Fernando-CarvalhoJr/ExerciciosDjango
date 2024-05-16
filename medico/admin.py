a157df579e45b7a0f2abdf7d7f8c34c8d8f7d7ff
Fernando <aluno103.23187201fernando@gmail.com>
2024-05-15 21:56:25 -0300

    criacao dos models do exercicio

--- medico/admin.py
+++ medico/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import medico, especialidade
 
 # Register your models here.
+admin.site.register(medico)
+admin.site.register(especialidade)
--- medico/models.py
+++ medico/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class medico(models.Model):
+    nome = models.CharField(max_length=30)
+    endereco = models.CharField(max_length=60)
+    telefone = models.CharField(max_length=11)
+    email = models.EmailField(max_length=50)
+    data_nascimento = models.DateField()
+    crm = models.IntegerField(max_length=6)
+    especialidade_id = models.IntegerField()
+
+
+class especialidade(models.Model):
+    nome = models.CharField(max_length=30)
+    descrição = models.CharField(max_length=300)
